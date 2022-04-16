3a72fc400a84912aa8b651d31ed56b99d2869269
Tohidul29 <tohidswfu@gmail.com>
2022-04-17 02:51:36 +0600

    about page content updated

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image'
-import image from '../images/me/Tohidul.JPG'
+import owner from '../images/me/Tohidul.JPG'
 
 const About = () => {
     return (
         <div>
             <div className='text-center mt-4'>
-                <Image roundedCircle src={image} width={300} height={300}></Image>
-                <div className='mt-4 text-dark fs-5 w-75 mx-auto'><p >Hello, my name's <b>MD Tohidul Islam.</b> I am from Bangladesh. Recently, I have finished my bachelor study from Southwest Forestry University. From my University time, when I was in 3rd year, I was bit interested in Web development. One of my classmates inspired me a lot to continue. I have few goals to do. Currently, I learn React to make front-end application. My next goal is to learn express.js, mongoDB, next.js</p></div>
-                
+                <Image roundedCircle src={owner} width={300} height={300}></Image>
+                <div className='mt-4 text-dark fs-5 w-75 mx-auto'>
+                    <p>Hello, my name's <b>MD Tohidul Islam.</b> I have a few goals to do. Currently, I learn React to make front-end applications, HTML, CSS, JavaScript, Bootstrap. My next goal is to learn express.js, MongoDB, next.js. I hope, I can finish it in next 3-4 months. That's my target.</p>
+                </div>
             </div>
         </div>
     );
