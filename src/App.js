f10f7954381c544b5c04b5cdb54cd29ce6c19a3e
Tohidul29 <tohidswfu@gmail.com>
2022-04-17 16:10:06 +0600

    register part added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './Component/About/About';
 import Blogs from './Component/Blogs/Blogs';
-import Footer from './Component/Footer/Footer';
 import Header from './Component/Header/Header';
 import Home from './Component/Home/Home/Home';
 import Login from './Component/Login/Login';
 import NotFound from './Component/NotFound/NotFound';
+import Register from './Component/Register/Register';
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,43 +1,42 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 
-const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-
-    const handleLogin = event => {
-        event.preventDefault();
-    }
+const Register = () => {
 
     const navigate = useNavigate();
-    const navigateRegister = navigate('/register');
+    const navigateLogin = navigate('/login');
 
     return (
         <div className='mt-4 w-50 mx-auto'>
-            <h2 className='text-center my-4 text-dark'>Danial's Dental Care Login</h2>
-            <Form onSubmit={handleLogin}>
+            <h2 className='text-center my-4 text-dark'>Danial's Dental Care Register</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Full Name" required />
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                    <Form.Control type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="dark" type="submit">
-                    Login
+                    Register
                 </Button>
             </Form>
             <div className='mt-3'>
-                <p>New to Danial's Dental Care? <Link to='/register' className='text-primary' onClick={navigateRegister}>Register</Link> </p>
+                <p>Already have an account? <Link to='/login' className='text-primary' onClick={navigateLogin}>login</Link> </p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
