fb884b50fd50eed6001ed2d138925a200f0c8140
Tohidul29 <tohidswfu@gmail.com>
2022-04-18 05:57:54 +0600

    fancy button and toast added in checkout page

--- src/Component/CheckOut/Checkout.js
+++ src/Component/CheckOut/Checkout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Button } from 'react-bootstrap';
 
 const Checkout = () => {
+    const handleConfirm = () => {
+        toast('Thanks for your confirmation. I will contact with you soon via Email.');
+    }
+
     const [user] = useAuthState(auth);
     let userDetail;
     if (user) {
-        console.log(user)
         userDetail = <div className='mt-5'>
             <img className='rounded-circle my-4' src={user.photoURL} alt="" /> <br />
             <h4>Email: {user.email}</h4>
@@ -17,6 +23,10 @@ const Checkout = () => {
             <h2 className='text-center my-4'>Thanks for choosing Danial's Dental Care</h2>
             <div className='text-center'>
                 {userDetail}
+            </div>
+            <ToastContainer></ToastContainer>
+            <div className='my-5 text-center'>
+                <Button className='btn btn-dark' onClick={handleConfirm}>Confirm your appointment</Button>
             </div>
         </div>
     );
